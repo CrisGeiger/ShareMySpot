02425ee4da5967a2dbadb4c282d6cbeb0c1cb7a5
Studium <Studium@192.168.2.101>
2018-03-09 21:38:51 +0100

    Die ersten Methoden entwickelt

--- ShareMySpot/src/java/sharemyspot/ejb/UserBean.java
+++ ShareMySpot/src/java/sharemyspot/ejb/UserBean.java
@@ -5,10 +5,53 @@
  */
 package sharemyspot.ejb;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import sharemyspot.jpa.User;
+
 /**
  *
  * @author Studium
  */
+        /**
+         * Die Userbean bietet verschiedene Methoden, um als Benutzer am Benutzerprofil zu bearbeiten 
+         */
+  
+@Stateless
 public class UserBean {
     
+     @PersistenceContext 
+     EntityManager em;
+     
+     public void delete(User user){ // Methode zum löschen eines Benutzers
+        this.em.remove(user);
+     }
+     
+     public void update(User user){ // Methode zum Aktualisieren eines Benutzersprofils
+         this.em.merge(user);
+     }
+     
+     public void signup(String username,String password)throws SignupException{
+         
+         if(em.find(User.class,username)!=null){
+             throw new SignupException("Benutzer ist bereits vergeben.");
+         }
+         User user= new User(username,password);
+         em.persist(user);
+     }
+     public void getCurrentUser(User user){
+         this.em.find(User.class, user);
+     }
+     
+     /**
+      * selbstgeschriebene SignupException erbt von Exception und 
+      * dient als Ausnahme für die Fehlermeldung beim Registrieren 
+      */
+     public class SignupException extends Exception{
+     
+         public SignupException(String message){
+             super(message);
+         }
+     }
 }
